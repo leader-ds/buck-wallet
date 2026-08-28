9ec98edbd4ce1afd697fa12163f23035c501914a
Buck <leader@leader.hu>
2026-08-28 20:38:07 +0200

    fix(identity): close macOS visible product identity

--- build-scripts/mac/notarize-mac.sh
+++ build-scripts/mac/notarize-mac.sh
@@ -2,10 +2,10 @@ echo "Create keychain profile"
 xcrun notarytool store-credentials "notarytool-profile" --apple-id "$1" --team-id "$2" --password "$3"
 
 echo "Creating temp notarization archive"
-ditto -c -k --keepParent "build/macos/Build/Products/Release/ywallet.app" "notarization.zip"
+ditto -c -k --keepParent "build/macos/Build/Products/Release/BUCK Wallet.app" "notarization.zip"
 
 echo "Notarize app"
 xcrun notarytool submit "notarization.zip" --keychain-profile "notarytool-profile" --wait
 
 echo "Attach staple"
-xcrun stapler staple "build/macos/Build/Products/Release/ywallet.app"
+xcrun stapler staple "build/macos/Build/Products/Release/BUCK Wallet.app"
